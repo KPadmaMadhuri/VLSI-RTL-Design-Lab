c1d206777eeec1793c94583ba9bacc31a9e959bd
kpmadd <pamarikandala@gmail.com>
2026-03-01 11:11:11 +0530

    Updated Getting started folder

---
+++ HDLBits/GettingStarted/Getting_Started.v
@@ -0,0 +1,28 @@
+/* Getting Started */
+//-------------------------------------------------------------
+//Getting Started - Step One
+//https://hdlbits.01xz.net/wiki/Step_one
+//Description - Assigning "High" to a variable
+
+/*
+module top_module( output one );
+
+// Insert your code here
+    assign one = 1'b1;
+
+endmodule
+*/
+
+//-------------------------------------------------------------
+
+//Output Zero - Zero
+//https://hdlbits.01xz.net/wiki/Zero
+//Description - If no value is assigned to a Wire, Simulation defaults in value to Zero
+
+/*
+module top_module(
+    output zero
+);// Module body starts after semicolon
+
+endmodule
+*/ 
